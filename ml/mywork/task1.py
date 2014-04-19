b2543617e6bb72e4aaf2080eb9c4e775b9ea31b5
Prateek Tandon <erprateek.vit@gmail.com>
2014-04-19 18:09:24 -0400

    Working task1

--- ml/mywork/task1.py
+++ ml/mywork/task1.py
@@ -12,8 +12,9 @@ we want to glean the following insights:
 """
 
 
-def wordCounts(filename, popularity):
+def wordCounts(filename):
 	import sklearn.feature_extraction.text
+	import nltk
 	raw_data = open(filename).readlines()
 	cv = sklearn.feature_extraction.text.CountVectorizer()
 	stopwords = nltk.corpus.stopwords.words('english')
@@ -24,20 +25,58 @@ def wordCounts(filename, popularity):
 	freqs2 = [(word, cv_matrix.getcol(idx).sum()) for word, idx in cv.vocabulary_.items()]
 	sorted_freqs = sorted (freqs, key = lambda x: -x[1])
 	sorted_freqs2 = sorted (freqs2, key = lambda x: -x[1])
+	"""
+	Consider adding filter words since it is online deals so words such as Online, .com, link are avoided
+	"""
 	return sorted_freqs,sorted_freqs2
 	#return "Real word", tfidf_word, "Unreal word", cv_word
 
-def typesOFGuitar()
+def typesOfGuitar(filename, list_guitar_types):
+	"""Generate bigrams
+	for all the indices where guitar occurs, get previos index and add the word if its not not useful.
 	"""
-	Generate bigrams
-	for all the indices where guitar occurs, get previos index and add the word if its not nonsense.
+	"""Should generalize by the type of subject searched for
+		Instead of hard coding guitar
 	"""
+	"""Remove the cv words category by the end"""
+	"""More specific can be made by making it a 3 gram and analyzing what follows
+		Should incorporate collocation in nltk. That might help in finding frequencies of the terms occurring more than once.
+	"""
+	"""should supply deals only containing guitars"""
+
+	import sklearn.feature_extraction.text
+	from nltk import pos_tag
+	deals = open(filename).readlines()
+	ngram_tfidf = sklearn.feature_extraction.text.TfidfVectorizer(ngram_range=(2, 2))
+	ngram_tfidf_matrix = ngram_tfidf.fit_transform(deals)
+	bigrams = ngram_tfidf.vocabulary_.keys()
+	types = set()
+	for word in bigrams:
+		if u"guitar" in word:
+			[first,second] = word.split()
+			if u"guitar" in second:
+            	#print word
+				first_tag,second_tag = pos_tag(word.split())
+				cur_word, pos = first_tag
+				if pos in ['JJ']:
+					types.add(cur_word)
+	if list_guitar_types:
+		set_guitar_types = set(list_guitar_types)
+		types.intersection_update(set_guitar_types)
+	return (types, len(types))
 
 def main():
+	"""Retrieving the location of the file"""
 	filename = "deals.txt"
-	(tfidfWords, cvWords) = wordCounts(filename)
-	max_word = tfidfWords[0]
-	min_word = tfidfWords[-1]
-	
+	path = "../data/"+filename
+	"""path describes where the corpus is located"""
+	(tfidfWords, cvWords) = wordCounts(path)
+	max_term, max_count = tfidfWords[0]
+	min_term, min_count = tfidfWords[-1]
+	(guitar_types, num_guitar_types) = typesOfGuitar(path, [])
+	print "Most common term in the deals", max_term
+	print "Least common term in the deals", min_term
+	print guitar_types, num_guitar_types
+
 if __name__ == '__main__':
 	main()
