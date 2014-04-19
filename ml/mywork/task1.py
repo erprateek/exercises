238a85030dc58657f8009bcb64ae34b53f699e6a
Prateek Tandon <erprateek.vit@gmail.com>
2014-04-19 15:03:09 -0400

    task 1 and 2

--- ml/mywork/task1.py
+++ ml/mywork/task1.py
@@ -10,3 +10,34 @@ we want to glean the following insights:
 3. How many types of guitars are mentioned across all the deals?
 
 """
+
+
+def wordCounts(filename, popularity):
+	import sklearn.feature_extraction.text
+	raw_data = open(filename).readlines()
+	cv = sklearn.feature_extraction.text.CountVectorizer()
+	stopwords = nltk.corpus.stopwords.words('english')
+	tfidfv = sklearn.feature_extraction.text.TfidfVectorizer(stop_words = stopwords)
+	cv_matrix = cv.fit_transform(raw_data)
+	tfidf_matrix = tfidfv.fit_transform(raw_data)
+	freqs = [(word, tfidf_matrix.getcol(idx).sum()) for word, idx in tfidfv.vocabulary_.items()]
+	freqs2 = [(word, cv_matrix.getcol(idx).sum()) for word, idx in cv.vocabulary_.items()]
+	sorted_freqs = sorted (freqs, key = lambda x: -x[1])
+	sorted_freqs2 = sorted (freqs2, key = lambda x: -x[1])
+	return sorted_freqs,sorted_freqs2
+	#return "Real word", tfidf_word, "Unreal word", cv_word
+
+def typesOFGuitar()
+	"""
+	Generate bigrams
+	for all the indices where guitar occurs, get previos index and add the word if its not nonsense.
+	"""
+
+def main():
+	filename = "deals.txt"
+	(tfidfWords, cvWords) = wordCounts(filename)
+	max_words = tfidfWords[0]
+	min_words = tfidfWords[-1]
+	
+if __name__ == '__main__':
+	main()
