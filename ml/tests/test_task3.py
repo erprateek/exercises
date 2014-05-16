a0e49bf5e33f3ccf191d64849e54c7538eba2801
Prateek Tandon <erprateek.vit@gmail.com>
2014-05-15 20:53:32 -0400

    tested multiple classifiers

---
+++ ml/tests/test_task3.py
@@ -0,0 +1,18 @@
+'''
+Created on Apr 21, 2014
+
+@author: Prateek
+'''
+import unittest
+from mywork.task3 import *
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
